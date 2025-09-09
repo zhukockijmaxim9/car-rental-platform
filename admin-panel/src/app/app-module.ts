c31d0781d2cfa41044340d661f77a533f3ca6960
zhukockijmaxim9 <zhukockijmaxim@gmail.com>
2025-09-09 16:56:19 +0400

    Add basic Angular admin panel

---
+++ admin-panel/src/app/app-module.ts
@@ -0,0 +1,22 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Dashboard } from './dashboard/dashboard';
+
+@NgModule({
+  declarations: [
+    App,
+    Dashboard
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ admin-panel/src/app/dashboard/dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: false,
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css'
+})
+export class Dashboard {
+
+}
